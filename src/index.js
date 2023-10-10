c4689b8ca6206f79bc7514b1438154f1c9d0dc29
Hudu <ahmedhudu21@gmail.com>
2023-10-10 03:15:09 +0200

    components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const div = document.getElementById("root");
+if (div) {
+  const root = ReactDOM.createRoot(div);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
