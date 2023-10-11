e692a4c1a7c3c50967dc31e9b023c5d184f6f21a
Hudu <ahmedhudu21@gmail.com>
2023-10-11 11:37:42 +0200

    style nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,65 @@
 import React from "react";
-import './styles.scss';
+import "./styles.scss";
+import retink from "../images/retink.jpeg";
+import * as AiIcons from "react-icons/ai";
+import * as RxIcons from "react-icons/rx";
+import * as PiIcons from "react-icons/pi";
 
-function Navbar() {
-  
+function Navbar({ toggle }) {
   return (
-    <div className="nav-container">
-      
-    </div>
+    <header>
+      <div className="search-input">
+        <input
+          type="text"
+          className="query"
+          name=""
+          placeholder="Searc for templates, projects, etc"
+        />
+        <div className="icons">
+          <AiIcons.AiOutlineSearch  className="lens"/>
+          <AiIcons.AiOutlineClose  className="close"/>
+        </div>
+      </div>
+
+      <div className="btn-container">
+        <button>
+          <p>Create Content</p>
+        </button>
+      </div>
+      <label htmlFor="switch" class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+      </label>
+      <div className="nav">
+        <ul className="icon">
+          <li>
+            <a onClick={toggle} href="/caledar" className="icon-calendar">
+              <RxIcons.RxCalendar />
+            </a>
+          </li>
+        </ul>
+        <ul className="icon">
+          <li>
+            <a onClick={toggle} href="/bell" className="icon-bell">
+              <PiIcons.PiBellLight />
+            </a>
+          </li>
+        </ul>
+        <ul className="icon">
+          <li>
+            <a onClick={toggle} href="/profile" className="icon-image">
+              <img
+                src={retink}
+                alt="client"
+                className="profile"
+                width={35}
+                height={35}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 
